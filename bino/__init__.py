7ad8f98b992dbabc770997c4ec8537def4a863b3
Kirill_Grishin <grischin.ka15@physics.msu.ru>
2019-12-07 14:31:44 +0300

    __init__.py and __main__.py added

---
+++ bino/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from .bino import *
+
+name = 'binospec_utils'
+version = '0.0.1'
---
+++ bino/__main__.py
@@ -0,0 +1,9 @@
+from __future__ import print_function, unicode_literals
+import bino_utils
+
+def main():
+    print('bino_utils version is {}'.format(ser.version))
+
+
+if __name__ == '__main__':
+    main()
