9fb5cc271cbdd4668c0aa9007de4f74aeb698fed
Kirill_Grishin <grischin.ka15@physics.msu.ru>
2019-12-20 12:39:45 +0300

    GAIA DR2 option added for query_text

--- bino/utils.py
+++ bino/utils.py
@@ -2,6 +2,7 @@ from astropy.table import Table, hstack
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 import numpy as np
+import os
 
 def cross_match(a_cat, b_cat, a_ra_name='ALPHA_J2000',
                 b_ra_name='RAJ2000', a_dec_name='DELTA_J2000',
@@ -22,4 +23,12 @@ def make_query(ra, dec, source='2M', r=0.3):
         s = "SELECT * FROM \"II/246/out\" WHERE CONTAINS(POINT('ICRS',RAJ2000,DEJ2000), CIRCLE('ICRS',%.5f,%.5f,%.5f))=1 AND Kmag>0 AND Jmag>0" % (ra, dec, r)
     if source == 'UK':
         s = "SELECT * FROM \"II/319/las9\" WHERE CONTAINS(POINT('ICRS',RAJ2000,DEJ2000), CIRCLE('ICRS',%.5f,%.5f,%.5f))=1 AND Kmag>0 AND Jmag1>0" % (ra, dec, r)
+    if source == 'GDR2':
+        s = "SELECT * FROM gaiadr2.gaia_source WHERE CONTAINS(POINT('ICRS', ra, dec), CIRCLE('ICRS',%.5f,%.5f,%.5f))=1" % (ra, dec, r)
     return s
+
+def cross_match_stilts(fnm, stilts_path='~/stilts.jar'):
+    stilts_command = "java -jar %s tskymatch2 ifmt1=fits ifmt2=fits find=best join=1and2 in1=%s#2 in2=pannstarrs_cat.fits ra1=ALPHA_J2000 dec1=DELTA_J2000 ra2=ra dec2=dec out=%s ofmt=fits error=14.5" % (stilts_path, fnm+'_astrometry_scatalog.fits', fnm+'_astrometry_matched.fits')
+    os.system(stilts_command)
+
+
