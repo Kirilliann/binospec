46808832a40b8570207597ac05972331a8873ae3
Kirill_Grishin <grischin.ka15@physics.msu.ru>
2019-12-21 10:38:42 +0300

    Main file added :)

---
+++ bino/bino.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+from .calc_zp_ir import calc_zp_ir
+from .check_astrometry_wcs import corr_wcs
+from .utils import *
+
+def main():
+    print("binospec/mmirs calibration module")
