85bd40c6d8474a8b036025ba2784fc7219b0f2fe
Kirill_Grishin <grischin.ka15@physics.msu.ru>
2019-12-21 10:37:15 +0300

    __init__ changed

--- bino/__main__.py
+++ bino/__main__.py
@@ -2,7 +2,7 @@ from __future__ import print_function, unicode_literals
 import bino_utils
 
 def main():
-    print('bino_utils version is {}'.format(ser.version))
+    print('bino_utils beta version')
 
 
 if __name__ == '__main__':
